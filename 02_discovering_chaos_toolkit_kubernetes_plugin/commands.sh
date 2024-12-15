dc6ee8a3c3345889abd3bf6ef5cd4a66ac3c3330
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-12-15 12:31:07 -0300

    02_discovering_chaos_toolkit_kubernetes_plugin

---
+++ 02_discovering_chaos_toolkit_kubernetes_plugin/commands.sh
@@ -0,0 +1,9 @@
+# Install or upgrade the chaostoolkit-kubernetes package
+pip install -U chaostoolkit-kubernetes
+
+# Discover the capabilities and features of the chaostoolkit-kubernetes package
+chaos discover chaostoolkit-kubernetes
+
+# Display the contents of the discovery.json file
+cat discovery.json
+            
