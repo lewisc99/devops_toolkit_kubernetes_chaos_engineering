f76d1fbf3406ea8e875cc9b8f10e9b81aa7fe447
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-12-15 12:45:16 -0300

    04_defining_steady_state_hypothesis

---
+++ 04_defining_steady_state_hypothesis/commands.sh
@@ -0,0 +1,11 @@
+# Display the contents of the terminate-pod.yaml file to see the configuration
+cat chaos/terminate-pod.yaml
+
+# Execute the Chaos Engineering experiment defined in the terminate-pod.yaml file
+chaos run chaos/terminate-pod.yaml
+
+# Check the exit status of the last command to verify if the experiment was successful (0 means success)
+echo $?
+
+# List all pods in the go-demo-8 namespace to observe the state of the pods after the chaos experiment
+kubectl --namespace go-demo-8 get pods
