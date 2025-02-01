bd84b6704d210e30b8ec555ee505cfe0f8aeb90d
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-02-01 17:40:09 -0300

    17_discovering_chaos_toolkit_istio_plugin/commands.sh

---
+++ 17_discovering_chaos_toolkit_istio_plugin/commands.sh
@@ -0,0 +1,8 @@
+# Install or upgrade the chaostoolkit-istio package
+pip install -U chaostoolkit-istio
+
+# Discover the capabilities of the chaostoolkit-istio plugin
+chaos discover chaostoolkit-istio
+
+# Display the contents of the discovery.json file
+cat discovery.json
